c87d43dfe8db13837fc244d3bf86249f02aaccf6
renyh <1365305148@qq.com>
2025-05-29 14:46:37 +0800

    feat:llm学习

---
+++ llm/owner_llm.py
@@ -0,0 +1,33 @@
+import random
+
+prompt = "春江"
+max_new_token = 50
+with open('ci.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+stoi = {ch: i for i, ch in enumerate(chars)}
+itos = {i: ch for i, ch in enumerate(chars)}
+
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: ''.join([itos[i] for i in l])
+
+transition = [[0 for _ in range(vocab_size)] for _ in range(vocab_size)]
+
+for i in range(len(text) - 1):
+    current_token_id = encode(text[i])[0]
+    next_token_id = encode(text[i + 1])[0]
+    transition[current_token_id][next_token_id] += 1
+
+generated_token = encode(prompt)
+
+for i in range(max_new_token - 1):
+    current_token_id = generated_token[-1]
+    logits = transition[current_token_id]
+    total = sum(logits)
+    logits = [logit / total for logit in logits]
+    next_token_id = random.choices(range(vocab_size), weights=logits, k=1)[0]
+    generated_token.append(next_token_id)
+    current_token_id = next_token_id
+print(decode(generated_token))
